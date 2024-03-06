71ad3da91fa5672320b3662071f90c0d065b6669
Luca Utzeri <lutzeri@libero.it>
2024-03-06 14:05:22 +0100

    Initial commit

---
+++ src/test/java/Luca/Utzeri/capstone/Project/CapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Luca.Utzeri.capstone.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
